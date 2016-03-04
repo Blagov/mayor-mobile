f6586ec16c3af0fe8d5f16b13b4f95656d734e82
Lubomir Blagov <l.k.blagov@gmail.com>
2016-03-04 22:53:58 +0000

    Changes made by Lubomir Blagov (l.k.blagov@gmail.com).

--- mayor/components/home/index.js
+++ mayor/components/home/index.js
@@ -1,11 +1,62 @@
-'use strict';
+'use strict'; 
 
 app.home = kendo.observable({
     onShow: function() {},
     afterShow: function() {}
 });
 
-// START_CUSTOM_CODE_home
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+ 
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function onDeviceReady() {
+	navigator.splashscreen.hide();
+    geolocationApp = new geolocationApp();
+	geolocationApp.run();
+    
+}
+ 
+function geolocationApp() {
+}
 
-// END_CUSTOM_CODE_home
+geolocationApp.prototype = {
+	_watchID:null,
+    
+	run:function() {
+		var that = this;
+		that._handleWatch.apply(that, arguments);
+	},    
+	_handleWatch:function() {
+		var that = this;
+        var options = {
+            frequency: 1000,
+            enableHighAccuracy: true
+        };
+        that._watchID = navigator.geolocation.watchPosition(function() {
+            that._onSuccess.apply(that, arguments);
+        }, function() {
+            that._onError.apply(that, arguments);
+        }, options);
+	},
+    
+	_onSuccess:function(position) {
+		// Successfully retrieved the geolocation information. Display it all.
+        
+		this._setResults('Latitude: ' + position.coords.latitude + '<br />' +
+						 'Longitude: ' + position.coords.longitude + '<br />');
+	},
+    
+	_onError:function(error) {
+		this._setResults('code: ' + error.code + '<br/>' +
+						 'message: ' + error.message + '<br/>');
+	},
+    
+	_setResults:function(value) {
+		if (!value) {
+			document.getElementById("results").innerHTML = "";
+		}
+		else {
+            //console.log(value);
+			$("#results").html(value);
+		}
+	},
+}

92ded99406a3e76f9717b752e25662a319f4e5fa
Lubomir Blagov <l.k.blagov@gmail.com>
2016-03-09 11:25:05 +0000

    Changes made by Lubomir Blagov (l.k.blagov@gmail.com).

--- mayor/components/formView/index.js
+++ mayor/components/formView/index.js
@@ -1,72 +1,171 @@
 'use strict';
-var el = new Everlive('ic4u9vd726cmn1ot');
 
-var el = new Everlive({
-    appId: "ic4u9vd726cmn1ot",
-    scheme: "https",
-    authentication: {
-            persist: true
-    }
-});
-
+var el = app.data.mayorMobile;
+var items = {
+    data: [],
+    pageSize: 0
+};
+var views = {
+    default: "start-report-view",
+    views: ["start-report-view", "prev-next", "steps-view", "photos-view", "location-view","comment-view","user-info-view"],
+    active: null
+}
 app.formView = kendo.observable({
     onShow: function () {
+        views.active = null;
         checkLogin();
+        initializeViews();
     },
     afterShow: function () {}
 });
 
-// START_CUSTOM_CODE_formView
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-function checkLogin() {
-el.Users.currentUser(function(data) {
-    if (data.result) {
-        var username = data.result.DisplayName;
-        alert(username + " is logged in!");
-    } else {
-         setTimeout(function() {
-         	app.mobileApp.navigate('components/authenticationView/view.html');
-         }, 0);    
-    }
-}, function(err) {
-    alert(err.message + " Please log in.");
-});
-}
-// END_CUSTOM_CODE_formView
 (function (parent) {
     var formViewModel = kendo.observable({
         fields: {},
         submit: function () {},
         cancel: function () {},
         takePhoto: function () {
-            navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+            navigator.camera.getPicture(onSuccess, onFail, {
                 quality: 50,
                 destinationType: Camera.DestinationType.DATA_URL
             });
+            views.active = 3;
+            initializeViews();
+        },
+        prev: function () {
+            if (!(views.active === 3)) {
+                views.active--;
+                initializeViews()
+                if (views.active === 4) {
+                    loadMap();
+                }
+            }
+        },
+        next: function () {
+            if (!(views.active === views.views.length - 1)) {
+                views.active++;
+                initializeViews()
+                if (views.active === 4) {
+                    loadMap();
+                }
+            }
         }
     });
-
-parent.set('formViewModel', formViewModel);
+    parent.set('formViewModel', formViewModel);
 })(app.formView);
 
-    function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64 encoded image data
-      // console.log(imageData);
+function checkLogin() {
+    el.Users.currentUser(function (data) {
+        if (data.result) {
+            var username = data.result.DisplayName;
+            alert(username + " is logged in!");
+        } else {
+            setTimeout(function () {
+                app.mobileApp.navigate('components/authenticationView/view.html');
+            }, 0);
+        }
+    }, function (err) {
+        alert(err.message + " Please log in.");
+    });
+};
 
-      // Get image handle
-      //
-      var smallImage = document.getElementById('smallImage');
+function onSuccess(imageData) {
 
-      // Unhide image elements
-      //
-      smallImage.style.display = 'block';
-
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
+    items.data.push({
+        img: imageData
+    });
+    items.pageSize = items.data.length;
+    console.log(items);
+    var ds = new kendo.data.DataSource(items);
+    var scrollView = $("#scrollview").data("kendoMobileScrollView");
+    scrollView.setDataSource(ds);
+    scrollView.refresh();
+    //var smallImage = document.getElementById('smallImage'); //<div data-role="page"><img style="width:100%" id="smallImage" src="" /></div>
+    //smallImage.src = "data:image/jpeg;base64," + imageData;
+}
 
 function onFail(message) {
     alert('Failed because: ' + message);
+}
+
+function initializeViews() {
+    var view;
+    views.active ? view = views.active : view = 0;
+    views.views.forEach(function (e, i) {
+        if (view === i) {
+            $('.' + views.views[i]).show();
+            if (views.active) {
+                $('.' + views.views[1]).show();
+                $('.' + views.views[2]).show();
+            }
+        } else {
+            $('.' + views.views[i]).hide();
+        }
+    })
+}
+
+function loadMap() {
+    var l = new geolocation();
+    var center, map, mapProp, mark;
+    var infowindow = new google.maps.InfoWindow({
+        content: "Hello World!"
+    });
+
+    navigator.splashscreen.hide();
+    if (navigator.geolocation.lastPosition) {
+        center = new google.maps.LatLng(navigator.geolocation.lastPosition.coords.latitude, navigator.geolocation.lastPosition.coords.longitude);
+        mapProp = {
+            center: center,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+    } else {
+        center = new google.maps.LatLng(42.697708, 23.321868);
+        mapProp = {
+            center: center,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+    }
+    map = new google.maps.Map(document.getElementById("map"), mapProp);
+    mark = new google.maps.Marker({
+        position: center,
+    });
+    mark.setMap(map);
+
+    google.maps.event.addListener(mark, 'click', function () {
+        infowindow.open(map, mark);
+    });
+    l.getLocation(function (position, error) {
+        if (error) {
+            var html = 'Err code: ' + error.code + '<br />' + 'message: ' + error.message + '<br />'
+        } else {
+            var c = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(c);
+            map.setZoom(15)
+            mark.setPosition(c);
+            geocode(c, function (results) {
+                console.log(results[0]);
+                infowindow.content = results[0].formatted_address;
+            });
+            var html = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />'
+        }
+    });
+}
+
+function geocode(coords, cb) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        'location': coords
+    }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[1]) {
+                cb(results);
+            } else {
+                //window.alert('No results found');
+            }
+        } else {
+            // window.alert('Geocoder failed due to: ' + status);
+        }
+    });
 }

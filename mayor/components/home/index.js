c29c1de47459914e4e936a6ea1a96eb3df230ae5
Alexander Makedonski <alex.makedonski93@gmail.com>
2016-03-22 20:46:04 +0000

    Changes made by Alexander Makedonski (alex.makedonski93@gmail.com).

--- mayor/components/home/index.js
+++ mayor/components/home/index.js
@@ -100,42 +100,6 @@ function showAroundReports(event) {
     })
 }
 
-function dateFormat(date) {
-
-    var mshours = 60 * 60 * 1000,
-        msDay = 60 * 60 * 24 * 1000,
-        msMinutes = 60 * 1000,
-        then = new Date(date),
-        now = new Date(),
-        string = '';
-
-    var days = Math.floor((now - then) / msDay),
-        hours = Math.floor(((now - then) % msDay) / mshours),
-        minutes = Math.floor(((now - then) % msDay) / msMinutes);
-    if (days > 0) {
-        days == 1 ? string += days + " ден," : string += days + " дни,";
-    }
-    if (mshours > 0) {
-        hours == 1 ? string += hours + " час" : string += hours + " часа";
-    }
-    if (minutes > 0 && minutes < 60) {
-        minutes == 1 ? string = minutes + " минута" : string = minutes + " минути";
-    }
-    if (days == 0 && hours == 0 && minutes == 0) {
-        var seconds = Math.floor((now - then) / 1000);
-        seconds == 1 ? string = seconds + " секунда" :  string = seconds + " секунди";
-    }
-
-    return string;
-}
-
-function getFollowers(f){
-    var r;
-    f.Followers != null ? r = f.Followers.length : r = 0;
-    return r;
-	
-}
-
 function onSelect(i) {
     app.mobileApp.navigate('components/selectedReport/view.html?id='+i);
 }
--- mayor/components/selectedReport/index.js
+++ mayor/components/selectedReport/index.js
@@ -13,6 +13,12 @@ app.selectedView = kendo.observable({
         var scrollView = $("#selectedReportView").data("kendoMobileScrollView");
         scrollView.setDataSource(ds);
         scrollView.refresh();
+
+        var html = "<h5> Време: #=dateFormat(data.CreatedAt)#</h5><h5>Категория: #=data.Category.Name# </h5><h5>Последователи: #=getFollowers(data) #</h5><h5>Адрес: #=data.Address#</h5><h5>Потребител: #=data.Owner.DisplayName#</h5><h5>Организация:  #=data.Region#</h5><h5> Коментар: #=data.Comment#</h5>";
+        var template = kendo.template(html);
+        var result = template(report);
+        var scroller = $("#reportInfo").data("kendoMobileScroller");
+        scroller.scrollElement.html(result);
     },
     afterShow: function () {}
 });

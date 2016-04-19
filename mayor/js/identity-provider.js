2f81efe2d81c32ae149989c9703d53c8c8e19c06
Alexander Makedonski <alex.makedonski93@gmail.com>
2016-04-19 20:28:56 +0000

    Changes made by Alexander Makedonski (alex.makedonski93@gmail.com).

--- mayor/js/identity-provider.js
+++ mayor/js/identity-provider.js
@@ -1,23 +1,17 @@
 function getParameterByName(name, url) {
     name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
     var regexS = name + '=([^&#]*)';
-
     var regex = new RegExp(regexS);
     var results = regex.exec(url);
-
     if (results === null) {
         return false;
     } else {
         return decodeURIComponent(results[1].replace(/\+/g, ' '));
     }
 }
-
 var IdentityProvider = function (config) {
     var that = this;
-
     this.getAccessToken = function(callback) {
-
-        // Begin Authorization
         var authorize_url = config.endpoint
                             + '?response_type=' + config.response_type
                             + '&client_id=' + config.client_id
@@ -25,27 +19,19 @@ var IdentityProvider = function (config) {
                             + '&display=' + config.display
                             + '&access_type=' + config.access_type
                             + '&scope=' + config.scope;
-
-        // open the InAppBrowser with the link 
         ref = window.open(authorize_url, '_blank', 'location=no');
-
         ref.addEventListener('loadstop', function(event) {
             that.locationChanged(event.url, callback);
         });
-
         ref.addEventListener('loaderror', function(event) {
             alert("Load error: " + event.message);
         });
-        
-        // The following is required in iPhone as the loadstop event is never fired.
-        // The check for Google is required to parse the access token of the redirect Uri 
         if (config.name === 'Google'){
             ref.addEventListener('loadstart', function(event) {
                 that.locationChanged(event.url, callback);
             });
         }
     }
-
     this.locationChanged = function(loc, callback) {
         if (loc.indexOf('access_token=') !== -1) {
             ref.close();
